bb09cf03acf0d4986583d2da5dba5028700f3b76
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2017-12-22 21:16:28 +0200

    Started working with subreddits

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from fbchat import Client
+import random
+import re
 import config
 
 
@@ -9,15 +11,25 @@ class Martzi(Client):
 
     def onMessage(self, message_object, author_id,
                   thread_id, thread_type, **kwargs):
-        if author_id != self.uid:
-            self.send(message_object, thread_id=thread_id,
-                      thread_type=thread_type)
+        msg = message_object.text
+        if author_id == self.uid or (not msg.startswith('!')):
+            return  # Ignore your messages
+        pattern = re.compile('!(.*) ?.*')
+        m = pattern.match(msg)
+        cmd = m.groups(0)[0]
+        print("I recieved command:", cmd)
+        self.execute_command(cmd)
+
+    def execute_command(self, cmd):
+        if cmd in subreddit.keys():
+            # Select a random subreddit
+            sub = subreddit[cmd][random.randint(0, len(subreddit[cmd]) - 1)]
 
 
 def login():
     try:
         print("Logging in...")
-        client = Martzi(username, password, logging_level=50)
+        client = Martzi(username, password, logging_level=30)
         print("Logged in successfully!")
         print("Listening...")
         client.listen()
